806cc509ddaec2a7704168dbcd05f99d66fb71f3
Dimitri Bocquet <contact@dimitribocquet.com>
2016-05-15 12:18:54 +0200

    create custom UIView class add shadow on MaterialView

---
+++ dimbook/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  dimbook
+//
+//  Created by Dimitri Bocquet on 15/05/2016.
+//  Copyright © 2016 Dimitri Bocquet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
